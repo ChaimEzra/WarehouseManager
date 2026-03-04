1c3c6fbe1b5524909b1939e73b89a8d27ce16586
ChaimEzra <chimas1357@gmail.com>
2026-03-04 16:03:26 +0200

    added the query Command

--- Commands/QueryCommand.cs
+++ Commands/QueryCommand.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WarehouseManager.QueryFolder;
+using WarehouseManager.WarehouseFolder;
 
 namespace WarehouseManager.Commands
 {
@@ -8,7 +11,35 @@ namespace WarehouseManager.Commands
     {
         public void Execute(string[] args)
         {
-            Console.WriteLine("Query");
+            string query = string.Join(" ", args);
+            Query q = new QueryParser().Parse(query);
+            try
+            {
+
+                var whereFunc = new WhereQuery().Execute(q.WherePart);
+                var selectFunc = new SelectQuery().Execute(q.SelectPart);
+                var orderbyFunc = new OrderbyQuery().Execute(q.OrderByPart);
+
+                var whereResult = whereFunc(Warehouse.GetWarehouse().ItemsInWarehouse.Keys);
+                var orderResult = orderbyFunc((IEnumerable<Item>)whereResult);
+                var selectResult = selectFunc((IEnumerable<Item>)orderResult);
+
+                if (selectResult is IEnumerable<object> enumerable)
+                {
+                    foreach (var item in enumerable)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(selectResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Information("Erorr in query command:" + ex.Message);
+            }
         }
     }
 }
